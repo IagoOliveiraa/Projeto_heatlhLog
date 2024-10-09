903bb70651aa9201ee149ed7c640ec78ddbe62c3
Anabialimaz <anabeatrizbarroslimaz@gmail.com>
2024-10-09 13:37:20 -0300

    biaa

--- HealthLog/src/Pages/EspecialidadesEsteticas.jsx
+++ HealthLog/src/Pages/EspecialidadesEsteticas.jsx
@@ -1,92 +1,126 @@
 import "../CSS/EspecialidadesEsteticas.css";
-import imagem200 from "../assets/grafico200.png";//importando imagem do gráfico
-import imagem89 from "../assets/grafico89.png";//importando imagem do gráfico
-import imagemhealthlog from "../assets/helthloggrafico.png";//importando imagem do gráfico
-import imagemmuitomais from "../assets/graficomuitomais.png";//importando imagem do gráfico
+import { Link } from "react-router-dom"; // Importa o Link do React Router
+import imagem200 from "../assets/grafico200.png";
+import imagem89 from "../assets/grafico89.png";
+import imagemhealthlog from "../assets/helthloggrafico.png";
+import imagemmuitomais from "../assets/graficomuitomais.png";
+import logoh from "../assets/Logo1.png";
+import { FaSearch } from 'react-icons/fa';
+import { FaRegCircleUser } from "react-icons/fa6";
+import { SlArrowDown, SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 function EspecialidadesEsteticas() {
-    return (
-        <div className="Estetica-container">
-            <header className="cabeçalho">
 
-                <div className="logo">
-                    <img src="" alt="Health Log Logo" />
-                    <h1>Health Log</h1>
-                </div>
+  const [busca, setBusca] = useState("");
+  const navegar = useNavigate();
+ 
+  const irParaPerfil = () => {
+    navegar("/perfil");
+  };
+ 
+  const pesquisar = (evento) => {
+    if (evento.key === "Enter") {
+      navegar(`/pesquisa/${busca}`);
+    }
+  };
+  
+  return (
+    <div className="containerAA">
 
-                <div className="pesquisa-login">
-                    <input type="text" placeholder="Buscar" />
-                    <a href="/Login">Login</a>
-                    <a href="/registro">Cadastra-se</a>
+<div className="barraAA">
+<img src={logoh} alt="Logo" className="logoAA" />
+<div className="barra-pesquisaAA">
+  <FaSearch className="icone-pesquisaAA" />
+  <input
+    type="text"
+    placeholder="Buscar"
+    className="input-pesquisaAA"
+    value={busca}
+    onChange={(e) => setBusca(e.target.value)}
+    onKeyPress={pesquisar}
+  />
+</div>    
+<div className="containerservicosAA">
+  <p className="divservicosAA">
+    Serviços
+    <SlArrowDown className="icone-setaAA" />
+  </p>
+</div>
+<FaRegCircleUser className="icone-perfilAA" onClick={irParaPerfil} />
+</div>
+      {/* Seção de imagens no topo */}
+      <div className="graficoAA">
+        <img src={imagem200} alt="grafico200" />
+        <img src={imagem89} alt="grafico89" />
+        <img src={imagemhealthlog} alt="graficohealthlog" />
+        <img src={imagemmuitomais} alt="graficomuitomais" />
+      </div>
 
-                </div>
+      {/* SEPARANDO CABEÇALHO*/}
 
-            </header>
+      <div className="titulo2AA">
+        <h1>Especialidades Odontológicas</h1>
+      </div>
 
-            <section className="conteudo-principal">
+      {/* SEPARANDO TITULO*/}
 
-                <div className="grafico">
-                    <img src={imagem200} alt="grafico200" />
-                    <img src={imagem89} alt="grafico89" />
-                    <img src={imagemhealthlog} alt="graficohealthlog" />
-                    <img src={imagemmuitomais} alt="graficomuitomais" />
+      {/* Grid das especialidades odontológicas */}
+      <div className="todas-caixas2AA">
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Ortodontia</h2>
+          <p>
+            Foca na correção de dentes e mandíbulas desalinhados usando
+            aparelhos e outros dispositivos
+          </p>
+        </Link>
 
-                </div>
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Endodontia</h2>
+          <p>
+            Trata de problemas no interior dos dentes, especialmente no
+            tratamento de canais radiculares
+          </p>
+        </Link>
 
-                <div className="titulo">
-                    <h1>Especialidades Estéticas</h1>
-                </div>
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Periodontia</h2>
+          <p>
+            Cuida das doenças das gengivas e dos tecidos de suporte dos dentes
+          </p>
+        </Link>
+      </div>
 
-                <div className="caixas">
+      <div className="todas-caixas2AA">
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Cirurgia Bucomaxilofacial</h2>
+          <p>
+            Envolve procedimentos cirúrgicos complexos para tratar condições da
+            boca, mandíbula e face
+          </p>
+        </Link>
 
-                    <div className="box1">
-                        <h3> Cirurgia <br /> Plástica <br /> </h3>
-                        <h4> Procedimentos cirúrgicos que modificam ou aprimoram a aparência física, como rinoplastia, lifting facial, e lipoaspiração</h4>
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Odontopediatria</h2>
+          <p>
+            Especializa-se no atendimento odontológico de crianças e
+            adolescentes
+          </p>
+        </Link>
 
-                    </div>
-
-
-                    <div className="box1">
-                        <h3>Dermatologia <br /> Estética <br /> </h3>
-                        <h4>Tratamentos de pele que visam melhorar a aparência, como preenchimentos faciais, botox, e peelings químicos</h4>
-
-                    </div>
-
-
-                    <div className="box1">
-                        <h3>Odontologia<br /> Estética <br /> </h3>
-                        <h4>Foca na melhoria da aparência dos dentes e do sorriso através de procedimentos como clareamento dental, facetas e aparelhos ortodônticos</h4>
-
-                    </div>
-
-
-                    <div className="box1">
-                        <h3>Tricologia <br /> Estética <br /> </h3>
-                        <h4>Tratamentos para cabelos e couro cabeludo, incluindo transplante capilar, tratamentos contra queda de cabelo e cuidados estéticos capilares</h4>
-
-                    </div>
-
-                    <div className="box1">
-                        <h3>Nutrição <br /> Estética<br /> </h3>
-                        <h4>Planejamento alimentar voltado para a melhoria da aparência da pele, cabelo e unhas, além de manutenção de um peso saudável</h4>
-
-                    </div>
-
-                    <div className="box1">
-                        <h3>Estética <br /> Corporal<br /> </h3>
-                        <h4>Procedimentos não invasivos para modelagem do corpo, como massagem modeladora, drenagem linfática e tratamentos de celulite</h4>
-
-                    </div>
-                </div>
-
-
-
-            </section>
-
-
-        </div>
-    )
+        <Link to="/ClinicasPertoDeVoce" className="caixa2AA">
+          <h2>Odontologia Restauradora</h2>
+          <p>
+            Foca em restaurar dentes danificados por cáries ou trauma, incluindo
+            o uso de restaurações e coroas
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
 
 export default EspecialidadesEsteticas;
