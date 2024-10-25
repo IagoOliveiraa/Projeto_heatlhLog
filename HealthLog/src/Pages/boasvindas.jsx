42216adaa5513fb53df9437ebca7bd49e6112805
diguin07 <diego.silverbox@gmail.com>
2024-10-25 14:56:16 -0300

    update for Diego

--- HealthLog/src/Pages/boasvindas.jsx
+++ HealthLog/src/Pages/boasvindas.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../assets/CSS/boasvindas.css';
 import imgFund from '../assets/img/cachFund.jpg';
 import logo from '../assets/img/Rectangle 205.svg'
@@ -21,12 +22,13 @@ function BemVindo() {
                 <div className="linha"></div>
             <div className="box-para-login">
                     <h1 className='title-box-login'>Marque a sua próxima consulta!</h1>
-                    <a href="/Login" className='bottom-box-login'>Login</a>
+                    <Link to="/Login" className='bottom-box-login'>Login</Link>
                     <h2 className='text-box-login'>Não tem login? <a href="/registro">cadastre - se</a></h2>
             </div>
             <div className="deseja-continuar">
-                <a href="/">Deseja continuar sem <font color="#2F80ED">login?</font> 🡪</a> 
-            </div>                                      
+                <a href="/Home">Deseja continuar sem <font color="#2F80ED">login?</font> 🡪</a> 
+               
+            </div>                                
             <span className='text-rodapé'>
                 <p className='CNPJ'>CNPJ: 47.854.666/0001-70</p>
                 <p>R. Direita, 955 - Vila Santo Antônio, Cotia - SP, 06708-280</p>
--- HealthLog/src/Router.jsx
+++ HealthLog/src/Router.jsx
@@ -1,25 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registro from "./Pages/Registro";
 import Home from "./Pages/Home";
-import BoasVindas from "./Pages/boasvindas"
 import Login from "./Pages/Login";
 import LoginClinic from "./Pages/LoginClinic"; 
 import Cadastro from "./Pages/Cadastro"
 import CadastroClinic from "./Pages/CadastroClinic"
 import BemVindo from "./Pages/boasvindas";
+import ClinicaCalendario from "./Pages/agendamento";
 
 
 const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
+            <Route path="/" element={<Home/>}/>
                 <Route path="/boas-vindas" element={<BemVindo/>}/>
-                <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/registro" element={<Registro/>}/>
                 <Route path="/loginClinic" element={<LoginClinic/>}/> 
                 <Route path="/cadastro" element={<Cadastro/>}/>
                 <Route path="/cadastroClinic" element={<CadastroClinic/>}/>
+                <Route path="/agendamento" element={<ClinicaCalendario/>}/>
+
             </Routes>
         </BrowserRouter>
 
