ca1da2ad6e36a566b42b3529e5697d53d9a2eb56
LilianFerreiraRossaaaa <Ferreirarosalilian@gmail.com>
2024-11-08 14:42:54 -0300

    atualizado lilian 08-11

--- HealthLog/src/Router.jsx
+++ HealthLog/src/Router.jsx
@@ -18,8 +18,8 @@ const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
-            <Route path="/" element={<Home/>}/>
-                <Route path="/boasVindas" element={<BoasVindas/>}/>
+            <Route path="/" element={<BoasVindas/>}/>
+            <Route path="/inicio" element={<Home/>}/>
                 <Route path="/registro" element={<Registro/>}/>
                 <Route path="/loginClinic" element={<LoginClinic/>}/> 
                 <Route path="/cadastro" element={<Cadastro/>}/>
