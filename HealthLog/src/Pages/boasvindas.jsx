b9a8c27ad7f81913a6c4b90ce2911d1c27a9ccd8
IagoOliveiraa <iagooliveira1806@gmail.com>
2024-11-27 16:03:21 -0300

    começando a puxar

--- HealthLog/src/Pages/boasvindas.jsx
+++ HealthLog/src/Pages/boasvindas.jsx
@@ -22,10 +22,10 @@ function BemVindo() {
             <div className="box-para-login">
                     <h1 className='title-box-login'>Marque a sua próxima consulta!</h1>
                     <a href="/Login" className='bottom-box-login'>Login</a>
-                    <h2 className='text-box-login'>Não tem login? <a href="/cadastro">cadastre - se</a></h2>
+                    <h2 className='text-box-login'>Não tem login? <a href="/registro">cadastre - se</a></h2>
             </div>
             <div className="deseja-continuar">
-                <a href="/inicio">Deseja continuar sem <font color="#2F80ED">login?</font> 🡪</a> 
+                <a href="/">Deseja continuar sem <font color="#2F80ED">login?</font> 🡪</a> 
             </div>                                      
             <span className='text-rodapé'>
                 <p className='CNPJ'>CNPJ: 47.854.666/0001-70</p>
--- HealthLog/src/Pages/home.jsx
+++ HealthLog/src/Pages/home.jsx
@@ -1,55 +1,25 @@
-import { useState, useEffect } from 'react';
-import { FaRegUserCircle, FaSearch } from 'react-icons/fa';
-import { SlArrowDown } from 'react-icons/sl';
-import '../CSS/Home.css';
-import logosite from '../assets/img/Logo1.png';
-import bemvindo from '../assets/img/boasvindasFoto.png';
-import especMedicas from '../assets/img/MedicinaFoto.png';
-import especOdont from '../assets/img/OdontoFoto.png';
-import especEstetica from '../assets/img/EsteticaFoto.png';
-import FotoSobre from '../assets/img/FotoSobre.png';
-import { useNavigate } from 'react-router-dom'; 
+
+import { useState, useEffect } from 'react'; // Hooks para gerenciar estado e efeitos colaterais
+import { FaRegUserCircle, FaSearch } from 'react-icons/fa'; // Ícones de usuário e pesquisa
+import { SlArrowDown } from 'react-icons/sl'; // Ícone de seta para dropdown
+import '../assets/CSS/home.css'; // Estilos CSS importados
+import logosite from '../assets/Logo1.png'; // Logo do site
+import bemvindo from '../assets/boasvindasFoto.png'; // Imagem da seção de boas-vindas
+import especMedicas from '../assets/Medicinafoto.png'; // Imagem de especialidade médica
+import especOdont from '../assets/OdontoFoto.png'; // Imagem de odontologia
+import especEstetica from '../assets/EsteticaFoto.png'; // Imagem de estética
+import FotoSobre from '../assets/FotoSobre.png'; // Imagem da seção "Sobre Nós"
+
+
 
 function Home() {
-  const [busca, setBusca] = useState('');
-  const [slideIndex, setSlideIndex] = useState(1);
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate(); 
+  const [busca, setBusca] = useState(''); // Estado para gerenciar o texto da barra de busca
+  const [slideIndex, setSlideIndex] = useState(1); // Estado para controlar o slide atual
 
-  // Função para verificar se o usuário está logado
-  const isLoggedIn = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      return true;
-    }
-    return false;
-  };
-
-  useEffect(() => {
-    // Verifica se o usuário está logado ao carregar a página
-    if (!isLoggedIn()) {
-      // Se não estiver logado, redireciona para a página de login
-      navigate('/login'); 
-    } else {
-      // Obter dados do usuário do backend
-      fetch('/user', {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setUser(data); // Armazena as informações do usuário
-        })
-        .catch((error) => {
-          console.error('Erro ao obter dados do usuário:', error);
-        });
-    }
-  }, []);
-
+  // Função para exibir os slides de avaliação
   const showSlides = (n) => {
-    let slides = document.getElementsByClassName('mySlides');
-    let dots = document.getElementsByClassName('dot');
+    let slides = document.getElementsByClassName('mySlides'); // Coleta os slides
+    let dots = document.getElementsByClassName('dot'); // Coleta os indicadores
 
     if (n > slides.length) {
       setSlideIndex(1);
@@ -59,50 +29,50 @@ function Home() {
       setSlideIndex(n);
     }
 
+    // Oculta todos os slides
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
 
+    // Remove a classe "active" de todos os indicadores
     for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(' active', '');
     }
 
+    // Exibe o slide atual e destaca o indicador correspondente
     slides[slideIndex - 1].style.display = 'block';
     dots[slideIndex - 1].className += ' active';
   };
 
+  // Efeito para fazer os slides avançarem automaticamente a cada 3 segundos
   useEffect(() => {
     const interval = setInterval(() => {
-      showSlides(slideIndex + 1);
-    }, 3000);
-    return () => clearInterval(interval);
+      showSlides(slideIndex + 1); // Avança para o próximo slide
+    }, 3000); // Intervalo de 3 segundos
+    return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
   }, [slideIndex]);
 
+  // Função de pesquisa acionada ao pressionar Enter
   const pesquisar = (e) => {
     if (e.key === 'Enter') {
-      console.log('Buscando:', busca);
+      console.log('Buscando:', busca); // Exibe o termo de busca no console
     }
   };
 
+  // Função para redirecionar para a página de perfil
   const irParaPerfil = () => {
-    if (user) {
-      window.location.href = '/perfil';
-    } else {
-      window.location.href = '/login';
-    }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setUser(null);
-    window.location.href = '/';
+    console.log('Indo para o perfil...');
   };
 
   return (
     <>
+    
       <header className="home-container">
+        {/* Cabeçalho com logo, barra de pesquisa, menu de serviços e ícone de perfil */}
+
+
         <div className="barra-home">
-          <img src={logosite} alt="LogoHome" className="logoHome" />
+          <img src={logosite} alt="LogoHome" className="logoHome" /> {/* Substituir logoh por logosite */}
           <div className="barra-pesquisaHome">
             <FaSearch className="icone-pesquisaHome" />
             <input
@@ -113,7 +83,7 @@ function Home() {
               onChange={(e) => setBusca(e.target.value)}
               onKeyPress={pesquisar}
             />
-          </div>
+          </div>    
           <div className="containerservicosHome">
             <p className="divservicosHome">
               Serviços
@@ -126,42 +96,39 @@ function Home() {
             </div>
           </div>
           <div className='PerfilHome'>
-            {user ? (
-              <>
-                <p>Bem-vindo, {user.email}!</p>
-                <button onClick={handleLogout}>Sair</button>
-              </>
-            ) : (
-              <a href="/login"><FaRegUserCircle className="icone-perfilHome" onClick={irParaPerfil} /></a>
-            )}
+            <a href="/Perfil"><FaRegUserCircle className="icone-perfilHome" onClick={irParaPerfil} /></a>
           </div>
         </div>
       </header>
 
       <div className='feijoes'>
-        <h1>Escolha um serviço  & <br />
-        reserve sua consulta</h1>
+          <h1>Escolha um serviço  & <br />
+          reserve sua consulta</h1>
+
       </div>
 
+      {/* Seção de boas-vindas */}
+      
       <div className="img-boasvindas">
         <img className='bemVindo' src={bemvindo} alt="Boas-vindas" /> 
       </div>
 
+      {/* Seção principal com avaliações e procedimentos */}
       <section className="conteudo-principal">
         <div className="review-section">
           <h3>Avaliações</h3>
           <div className="slideshow-container">
             <div className="mySlides fade">
               <p><strong>Emmy Bubble</strong></p>
-              <p>Adorei usar o Health Log para agendar consultas...</p>
+              <p>Adorei usar o Health Log para agendar consultas. A interface é intuitiva e os lembretes são muito úteis. Recomendo!</p>
             </div>
             <div className="mySlides fade">
               <p><strong>John Smith</strong></p>
-              <p>A plataforma facilitou muito o processo de agendamento...</p>
+              <p>A plataforma facilitou muito o processo de agendamento de consultas. Uso e recomendo!</p>
             </div>
             <div className="mySlides fade">
               <p><strong>Aghata Montgomery</strong></p>
-              <p>Ótimo serviço! Acesso rápido e fácil...</p>
+              <p>Ótimo serviço! Acesso rápido e fácil. Continuarei utilizando para minhas consultas.</p>
             </div>
           </div>
           <div style={{ textAlign: 'center' }}>
@@ -171,6 +138,7 @@ function Home() {
           </div>
         </div>
 
+        {/* Seção de procedimentos mais buscados */}
         <div className="procedimentos">
           <h3>Procedimentos mais buscados</h3>
           <div className="service-items">
@@ -193,18 +161,54 @@ function Home() {
               <a href="/Clinicaspertodevoce"><button>Marcar agendamento</button></a>
             </div>
           </div>
+
+          {/* Seção de serviços */}
+          <div className="Servicos">
+            <h3>Serviços disponíveis</h3>
+            <div className="service-items">
+              <a href="/EspecialidadesMedicas" className="service-link">
+                <div className="service-item">
+                  <img src={especMedicas} alt="Especialidades Médicas" />
+                  <span>Medicina</span>
+                </div>
+              </a>
+              <a href="/EspecialidadesOdontologicas" className="service-link">
+                <div className="service-item">
+                  <img src={especOdont} alt="Especialidades Odontológicas" />
+                  <span>Odontologia</span>
+                </div>
+              </a>
+              <a href="/EspecialidadesEsteticas" className="service-link">
+                <div className="service-item">
+                  <img src={especEstetica} alt="Especialidades Estéticas" />
+                  <span>Esteticista</span>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </section>
 
+      {/* Rodapé */}
       <footer className="footer">
         <h3>Sobre Nós</h3>
         <p>
-          Health Log é uma plataforma inovadora projetada para simplificar o agendamento de consultas...
+          Health Log é uma plataforma inovadora projetada para simplificar o <br /> agendamento
+          de consultas em diversas áreas da saúde, incluindo <br /> médicos, clínicas de estética e
+          dentistas. Através de uma interface <br /> intuitiva, os usuários podem facilmente encontrar
+          profissionais <br /> disponíveis, verificar horários e marcar compromissos de forma <br /> rápida e eficiente,
+          tudo em um só lugar.
         </p>
+
         <img src={FotoSobre} alt="Sobre nós" />
+
         <div className='rodape'>
-          <p>2024 © Health Log - Todos os Direitos Reservados.</p>
+
+        <p>2024 © Health Log - Todos os Direitos Reservados.</p>
+
         </div>
+       
+      
       </footer>
     </>
   );
